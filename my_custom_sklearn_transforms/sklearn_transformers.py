1ce77e910d11eb0459574c1429bba36685715d7e
ibecer <irving.becerra121@gmail.com>
2020-08-21 09:44:16 -0500

    Add class AddProfile

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,18 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+
+class AddProfile(BaseEstimator, TransformerMixin):
+    def __init__(self, column, newColumn):
+        self.column = column
+        self.newColumn = newColumn
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X ):
+        # Primero copiamos el dataframe de datos de entrada 'X'
+        data = X.copy()
+        # Devolvemos un nuevo dataframe de datos sin las columnas no deseadas
+        return data.assign(PROFILE=self.newColumn)
